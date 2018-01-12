27419221d0a6e8dca950af3a28402057445174d4
uwe <uwe.krien@rl-institut.de>
2018-01-12 16:46:49 +0100

    remove underscore

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='oemof_visio',
+setup(name='oemofvisio',
       version='v0.0.1',
       author='oemof developing group',
       author_email='oemof@rl-institut.de',
       description='The open energy modelling framework',
       url='https://oemof.org/',
-      namespace_package=['oemof_visio'],
+      namespace_package=['oemofvisio'],
       long_description=read('README.rst'),
       install_requires=['matplotlib'])
